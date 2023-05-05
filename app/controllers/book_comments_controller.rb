d855c67a6d61e7c8d81f1c503ed5905f7264822b
popmash <131745061+popmash@users.noreply.github.com>
2023-05-05 13:14:51 +0000

    Update

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,22 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+  end
+  
+  def destroy
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+  end
+    
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:book_id, :user_id, :comment)
+  end
+
+end
